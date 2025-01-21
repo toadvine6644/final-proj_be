20aad14d92a1938e7a2edc0760a883f66047dfd5
toadvine6644 <tikkboi@gmail.com>
2025-01-21 09:35:20 +0700

    fix

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
